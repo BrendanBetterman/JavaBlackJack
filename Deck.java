df99a65eee7781d89c1472e0c668d80c44849dea
BrendanBetterman <58009876+BrendanBetterman@users.noreply.github.com>
2021-10-14 21:54:10 -0500

    Add files via upload

---
+++ Deck.java
@@ -0,0 +1,38 @@
+package BlackJackGame;
+import java.util.Random;
+public class Deck{
+    private String[] suit = {"S","D","C","H"};
+    private String[] cards = {"A","J","K","Q","2","3","4","5","6","7","8","9","10"};
+    private int numberOfDecks = 4;
+
+    public String[] deck = new String[suit.length * cards.length];
+    public int[] drawn = new int[deck.length];
+    
+    public Deck(){
+        for(int s = 0; s < this.suit.length; s ++){
+            for (int c = 0; c < this.cards.length; c++){
+                this.deck[this.cards.length*s + c] = ( this.cards[c] +" "+ this.suit[s]);
+            }
+        }
+    }
+    public void numOfDecks(int temp){
+        this.numberOfDecks = temp;
+    }
+    public void shuffle(){
+        for(int i = 0; i < this.drawn.length; i++){
+            this.drawn[i] = 0;
+        }
+    }
+    public String drawCard(){
+        Random random = new Random();
+        int tempCard = random.nextInt(deck.length);
+        if (this.drawn[tempCard] < this.numberOfDecks){
+            this.drawn[tempCard] ++;
+            return this.deck[tempCard];
+        }else{
+            return this.drawCard();
+        }
+    }
+}
+
+
